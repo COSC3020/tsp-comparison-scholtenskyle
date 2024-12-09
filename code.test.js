094262cb585c5c0317e8292229af8929449f2fef
scholtenskyle <35465569+scholtenskyle@users.noreply.github.com>
2024-12-09 10:01:12 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -10,7 +10,8 @@ function runTests() {
         [[0, 1, 2], [2, 0, 2], [0, 2, 1]],
         [[0, 4, 4, 5, 1], [2, 0, 3, 6, 6], [1, 4, 6, 5, 8], [2, 7, 5, 1, 3], [4, 3, 7, 2, 0]],
         Array.from({ length: 10 }, () => Array.from( { length: 10 }, () => Math.floor(Math.random() * 100))),
-        Array.from({ length: 7000 }, () => Array.from( { length: 7000 }, () => Math.floor(Math.random() * 15))),
+        //Array.from({ length: 21 }, () => Array.from( { length: 21 }, () => Math.floor(Math.random() * 15))),
+        //Array.from({ length: 7000 }, () => Array.from( { length: 7000 }, () => Math.floor(Math.random() * 15))),
     ];
 
     console.log("Test Results");
